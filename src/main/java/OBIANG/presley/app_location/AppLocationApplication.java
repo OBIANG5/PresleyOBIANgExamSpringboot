a3817d760324d1712895ad47729debac975a7fae
Presley Obiang <obiangabaghapresley@gmail.com>
2024-11-14 09:57:16 +0100

    Initial commit

---
+++ src/main/java/OBIANG/presley/app_location/AppLocationApplication.java
@@ -0,0 +1,13 @@
+package OBIANG.presley.app_location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/OBIANG/presley/app_location/AppLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package OBIANG.presley.app_location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
